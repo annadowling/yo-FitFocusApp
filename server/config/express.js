8a126a72abe047685e48fab8e20abfa4be4769c3
annadowling <anna.dowling@errigal.com>
2016-05-14 22:01:30 +0100

    edited express.js

--- server/config/express.js
+++ server/config/express.js
@@ -30,8 +30,8 @@ module.exports = function(app) {
   app.use(passport.initialize());
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', path.join(config.root, 'public'));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', path.join(config.root, 'client'));
     app.use(morgan('dev'));
   }
 
