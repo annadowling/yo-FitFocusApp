b99a253d9a73841123ac4a3b7f3624f6d60693a2
annadowling <anna.dowling@errigal.com>
2016-05-14 20:08:00 +0100

    Added prod env options

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
+//mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/yofitfocusapp');
 mongoose.connection.on('error', function(err) {
 	console.error('MongoDB connection error: ' + err);
 	process.exit(-1);
