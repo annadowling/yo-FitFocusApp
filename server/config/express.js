13b4b5dace360d75dfab41f32510e3eeedf7009e
annadowling <anna.dowling@errigal.com>
2016-05-14 21:58:31 +0100

    edited express.js

--- server/config/express.js
+++ server/config/express.js
@@ -29,11 +29,9 @@ module.exports = function(app) {
   app.use(cookieParser());
   app.use(passport.initialize());
   if ('production' === env) {
-    //app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'client')));
-    app.set('appPath', path.join(config.root, 'client'));
-    //app.use(express.static(path.join(config.root, 'public')));
-    //app.set('appPath', path.join(config.root, 'public'));
+    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
+    app.use(express.static(path.join(config.root, 'public')));
+    app.set('appPath', path.join(config.root, 'public'));
     app.use(morgan('dev'));
   }
 
